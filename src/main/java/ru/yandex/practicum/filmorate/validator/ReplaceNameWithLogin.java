36797a75d04bd6917718a270730dfbcea49ae17b
Jasah <Shjiwa@yandex.ru>
2023-03-23 02:58:36 +0300

    test: add annotation validator for name to replace it with login if null or blank

---
+++ src/main/java/ru/yandex/practicum/filmorate/validator/ReplaceNameWithLogin.java
@@ -0,0 +1,22 @@
+package ru.yandex.practicum.filmorate.validator;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(
+        validatedBy = {ReplaceNameWithLoginValidator.class}
+)
+public @interface ReplaceNameWithLogin {
+
+    String message() default "Null or blank name replaced with login.";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+}
