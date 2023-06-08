17db629bec8ddd9cfbf5889d83d2f717a3eef372
Jasah <Shjiwa@yandex.ru>
2023-06-08 21:57:31 +0300

    feat: add new features: add/delete like, get top 10 films

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,50 +1,90 @@
 package ru.yandex.practicum.filmorate.controller;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
-import ru.yandex.practicum.filmorate.exception.AlreadyExistException;
-import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.service.ExceptionService;
+import ru.yandex.practicum.filmorate.service.FilmService;
+import ru.yandex.practicum.filmorate.storage.film.FilmStorage;
+import ru.yandex.practicum.filmorate.storage.user.UserStorage;
 
 import javax.validation.Valid;
-import java.util.ArrayList;
-import java.util.LinkedHashMap;
 import java.util.List;
-import java.util.Map;
 
 @Slf4j
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/films")
 public class FilmController {
-    private final Map<Integer, Film> films = new LinkedHashMap<>();
-    private int id = 1;
+
+    private final FilmStorage filmStorage;
+    private final UserStorage userStorage;
+    private final FilmService filmService;
+    private final ExceptionService exceptionService;
 
     @GetMapping
     public List<Film> getFilms() {
-        log.info("Количество фильмов в текущий момент: {}", films.size());
-        return new ArrayList<>(films.values());
+        log.info("Поступил запрос на получение списка фильмов");
+        return filmStorage.getFilms();
     }
 
     @PostMapping
     public Film addFilm(@Valid @RequestBody Film film) {
-        if (films.containsValue(film)) {
-            log.error("Already exist.");
-            throw new AlreadyExistException("Already exist.");
-        }
-        film.setId(id++);
-        films.put(film.getId(), film);
-        log.info("Добавлен фильм: {} с id {}", film.getName(), film.getId());
-        return film;
+        log.info("Фильм {} добавлен", film.getName());
+        return filmStorage.addFilm(film);
     }
 
     @PutMapping
     public Film updateFilm(@Valid @RequestBody Film film) {
-        if (!films.containsKey(film.getId())) {
-            log.error("Not found.");
-            throw new NotFoundException("Not found.");
+        if (filmStorage.getMap().get(film.getId()) == null) {
+            log.error("Film not found");
+            exceptionService.throwNotFound();
         }
-        films.put(film.getId(), film);
-        log.info("Обновлен фильм: {} с id {}", film.getName(), film.getId());
-        return film;
+        log.info("Фильм c id {} обновлен", film.getId());
+        return filmStorage.updateFilm(film);
+    }
+
+    @PutMapping("/{id}/like/{userId}")
+    public Film addLike(@PathVariable Long id, @PathVariable Long userId) {
+        if (filmStorage.getMap().get(id) == null || userStorage.getMap().get(userId) == null) {
+            log.error("Film or user not found");
+            exceptionService.throwNotFound();
+        }
+        log.info("Пользователь {} поставил лайк фильму {}",
+                userStorage.getUserById(userId).getName(), filmStorage.getFilmById(id).getName());
+        return filmService.addLike(id, userId);
+    }
+
+    @DeleteMapping("/{id}/like/{userId}")
+    public Film deleteLike(@PathVariable Long id, @PathVariable Long userId) {
+        if (filmStorage.getMap().get(id) == null || userStorage.getMap().get(userId) == null
+                || !filmStorage.getFilmById(id).getLikes().contains(userId)) {
+            log.error("Film or user not found, or there is no like to delete");
+            exceptionService.throwNotFound();
+        }
+        log.info("Пользователю {} больше не нравится фильм {}",
+                userStorage.getUserById(userId).getName(), filmStorage.getFilmById(id).getName());
+        return filmService.deleteLike(id, userId);
+    }
+
+    @GetMapping("/{id}")
+    public Film getFilmById(@PathVariable Long id) {
+        if (filmStorage.getMap().get(id) == null) {
+            log.error("Film not found");
+            exceptionService.throwNotFound();
+        }
+        log.info("Поступил запрос на получение фильма с id {}", id);
+        return filmStorage.getFilmById(id);
+    }
+
+    @GetMapping("/popular")
+    public List<Film> getTop10Films(@RequestParam(defaultValue = "10") int count) {
+        if (count < 0) {
+            log.error("Negative count");
+            exceptionService.throwBadRequest();
+        }
+        log.info("Поступил запрос на получение списка топ {} популярных фильмов", count);
+        return filmService.getTop10Films(count);
     }
 }
