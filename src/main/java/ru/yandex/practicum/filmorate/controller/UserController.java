a5dd3dc858cc84427c85c87011d93b3e0e6350f4
Jasah <Shjiwa@yandex.ru>
2023-06-10 22:25:27 +0300

    refactor: move user storage bean to UserService

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.service.ExceptionService;
 import ru.yandex.practicum.filmorate.service.UserService;
-import ru.yandex.practicum.filmorate.storage.user.UserStorage;
 
 import javax.validation.Valid;
 import java.util.List;
@@ -16,73 +15,69 @@ import java.util.List;
 @RestController
 @RequestMapping("/users")
 public class UserController {
-    private final UserStorage userStorage;
     private final UserService userService;
     private final ExceptionService exceptionService;
 
     @PostMapping
     public User create(@Valid @RequestBody User user) {
-        log.info("Создан пользователь: {} с id {}", user.getName(), user.getId());
-        return userStorage.createUser(user);
+        log.info("Поступил запрос на создание пользователя: {}", user.getName());
+        return userService.createUser(user);
     }
 
     @GetMapping
     public List<User> getUsers() {
         log.info("Поступил запрос на получение списка пользователей");
-        return userStorage.getUsers();
+        return userService.getUsers();
     }
 
     @PutMapping
     public User update(@Valid @RequestBody User user) {
-        if (userStorage.getMap().get(user.getId()) == null) {
+        if (userService.getUserById(user.getId()) == null) {
             exceptionService.throwNotFound();
         }
-        log.info("Пользователь с id {} обновлен", user.getId());
-        return userStorage.updateUser(user);
+        log.info("Поступил запрос на обновление пользователя с id: {}", user.getId());
+        return userService.updateUser(user);
     }
 
     @PutMapping("/{id}/friends/{friendId}")
     public User addFriend(@PathVariable Long id, @PathVariable Long friendId) {
-        if (userStorage.getMap().get(id) == null || userStorage.getMap().get(friendId) == null) {
+        if (userService.getUserById(id) == null || userService.getUserById(friendId) == null) {
             exceptionService.throwNotFound();
         }
-        log.info("{} и {} теперь друзья",
-                userStorage.getUserById(id).getName(), userStorage.getUserById(friendId).getName());
+        log.info("Поступил запрос на добавление в друзья пользователей с id: {} и {}", id, friendId);
         return userService.addFriend(id, friendId);
     }
 
     @DeleteMapping("/{id}/friends/{friendId}")
-    public  User deleteFriend(@PathVariable Long id, @PathVariable Long friendId) {
-        if (userStorage.getMap().get(id) == null || userStorage.getMap().get(friendId) == null) {
+    public User deleteFriend(@PathVariable Long id, @PathVariable Long friendId) {
+        if (userService.getUserById(id) == null || userService.getUserById(friendId) == null) {
             exceptionService.throwNotFound();
         }
-        log.info("{} и {} перестали быть друзьями", userStorage.getUserById(id).getName(),
-                userStorage.getUserById(friendId).getName());
+        log.info("От пользователя с id: {} поступил запрос на удаление из друзей пользователя с id: {}", id, friendId);
         return userService.deleteFriend(id, friendId);
     }
 
     @GetMapping("/{id}/friends/common/{friendId}")
     public List<User> getSharedFriendsList(@PathVariable Long id, @PathVariable Long friendId) {
-        if (userStorage.getMap().get(id) == null || userStorage.getMap().get(friendId) == null) {
+        if (userService.getUserById(id) == null || userService.getUserById(friendId) == null) {
             exceptionService.throwNotFound();
         }
-        log.info("Поступил запрос на получение списка общих друзей пользователей {} и {}",
-                userStorage.getUserById(id), userStorage.getUserById(friendId));
+        log.info("Поступил запрос на получение списка общих друзей пользователей с id: {} и {}", id, friendId);
         return userService.getSharedFriendsList(id, friendId);
     }
 
     @GetMapping("/{id}")
     public User getUserById(@PathVariable Long id) {
-        if (userStorage.getMap().get(id) == null) {
+        if (userService.getUserById(id) == null) {
             exceptionService.throwNotFound();
         }
         log.info("Поступил запрос на получение пользователя с id {}", id);
-        return userStorage.getUserById(id);
+        return userService.getUserById(id);
     }
 
     @GetMapping("/{id}/friends")
     public List<User> getFriends(@PathVariable Long id) {
-        if (userStorage.getMap().get(id) == null) {
+        if (userService.getUserById(id) == null) {
             exceptionService.throwNotFound();
         }
         log.info("Поступил запрос на получение списка друзей пользователя с id {}", id);
