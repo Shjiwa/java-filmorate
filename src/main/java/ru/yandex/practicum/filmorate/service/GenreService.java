687286cb38be187080e3c47179e63b0ea33b3a5c
Jasah <Shjiwa@yandex.ru>
2023-06-15 08:56:26 +0300

    feat: add GenreService

---
+++ src/main/java/ru/yandex/practicum/filmorate/service/GenreService.java
@@ -0,0 +1,23 @@
+package ru.yandex.practicum.filmorate.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import ru.yandex.practicum.filmorate.model.Genre;
+import ru.yandex.practicum.filmorate.storage.genre.GenreDbStorage;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class GenreService {
+
+    private final GenreDbStorage genreDbStorage;
+
+    public List<Genre> getGenres() {
+        return genreDbStorage.getGenres();
+    }
+
+    public Genre getGenre(int id) {
+        return genreDbStorage.getGenreById(id);
+    }
+}
